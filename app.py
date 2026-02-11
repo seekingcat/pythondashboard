8f18bce1950740bbf175d78baff8a993502fedf7
Melanie Hudson <llana_a154@yahoo.co.uk>
2026-02-11 10:29:02 +0000

    added submit button to track each habit

--- app.py
+++ app.py
@@ -20,11 +20,18 @@ quotes = quote_res.json()
 
 stocks_res = requests.get(stock_url)
 stocks = stocks_res.json()
-# stock_data = stocks['Meta Data']['3. Last Refreshed']
+stock_date = stocks['Meta Data']['3. Last Refreshed']
 
 def load_habits():
     with open('habits.json', 'r') as f:
         return json.load(f)
+    
+# def write_habits(new_data, filename='habits.json'):
+#     with open('habits.json', 'r+') as f:
+#         f_data = json.load(f)
+#         f_data['date_completed'].append(new_data)
+#         f.seek(0)
+#         json.dump(f_data, f)
 
 def calculate_streak(dates_completed):
     if not dates_completed:
@@ -35,10 +42,12 @@ def calculate_streak(dates_completed):
 
     return 0
 
+
+
 @app.route('/')
 def dashboard():
     habits_data = load_habits()
-    
+
     data = {
             'current_time': datetime.now().strftime('%I:%M %p'),
             'current_date': datetime.now().strftime('%A, %B, %d, %Y'),
@@ -47,10 +56,10 @@ def dashboard():
             'feels': weather['main']['feels_like'],
             'quote':quotes[0]['q'],
             'author': quotes[0]['a'],
-            # 'stock_date':stocks['Meta Data']['3. Last Refreshed'],
-            # 'stock_name': stocks['Meta Data']['2. Symbol'],
-            # 'open': stocks['Time Series (Daily)'][stock_date]['1. open'],
-            # 'close': stocks['Time Series (Daily)'][stock_date]['4. close'],
+            'stock_date':stocks['Meta Data']['3. Last Refreshed'],
+            'stock_name': stocks['Meta Data']['2. Symbol'],
+            'open': stocks['Time Series (Daily)'][stock_date]['1. open'],
+            'close': stocks['Time Series (Daily)'][stock_date]['4. close'],
             'habits': habits_data['habits']
         }
 
